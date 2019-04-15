e89debe08e9ce8dcff4eb5bc79427984dbca521a
Nilton Lopes <niltonrck@gmail.com>
2019-04-15 11:43:54 -0300

    Start no projeto base e configuração do lint

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,60 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-layout-header>
+      <q-toolbar color="primary" :glossy="$q.theme === 'mat'" :inverted="$q.theme === 'ios'">
+        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu">
+          <q-icon name="menu"/>
+        </q-btn>
+
+        <q-toolbar-title>
+          App title
+          <div slot="subtitle">Subtitle</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-layout-drawer
+      v-model="leftDrawerOpen"
+      :content-class="$q.theme === 'mat' ? 'bg-grey-2' : null"
+    >
+      <q-list no-border link inset-delimiter>
+        <q-list-header>Essential Links</q-list-header>
+        <q-item v-for="(item, index) in drawerItems" :key="index" :to="item.destination">
+          <q-item-side :icon="item.icon"/>
+          <q-item-main :label="item.label" :sublabel="item.sublabel"/>
+        </q-item>
+      </q-list>
+    </q-layout-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      drawerItems: [
+        {
+          label: 'Menu item',
+          sublabel: ' sublabel',
+          destination: 'home',
+          icon: 'home'
+        }
+      ]
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
